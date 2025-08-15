b192282fd1427f19f9ea36db79a81383061ae6db
reeta-042 <155265356+reeta-042@users.noreply.github.com>
2025-08-15 10:51:13 +0100

    Update vectorbase.py

--- app/vectorbase.py
+++ app/vectorbase.py
@@ -3,7 +3,9 @@
 
 import os
 import pinecone
-from langchain.vectorstores import Pinecone
+from pinecone import Index
+#from langchain.vectorstores import Pinecone
+from langchain_pinecone import Pinecone as LangChainPinecone
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -18,14 +20,14 @@ def store_chunks(chunks, api_key, env, index_name):
     Returns the vectorstore instance.
     """
     # Initialize Pinecone
-    pc = Pinecone(api_key=api_key, environment=env)
-    index = pc.Index(index_name = index_name)
+    pinecone.init(api_key=api_key, environment=env)
+    index = Index(index_name = index_name)
 
     # Embedding model
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
     # Wrap Pinecone with LangChain
-    vectorstore = Pinecone(index, embedding_function=embeddings.embed_query, text_key="text")
+    vectorstore = LangChainPinecone(index, embedding_function=embeddings.embed_query, text_key="text")
 
     # Convert chunks to LangChain documents
     from langchain.docstore.document import Document
@@ -52,10 +54,10 @@ def get_vectorstore(api_key, env, index_name):
     """
     Loads the existing Pinecone index.
     """
-    pc = Pinecone(api_key= api_key, environment= env)
-    index = pc.Index(index_name = index_name)
+    pinecone.init(api_key= api_key, environment= env)
+    index = Index(index_name = index_name)
 
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    vectorstore = Pinecone(index, embedding_function=embeddings.embed_query, text_key="text")
+    vectorstore = LangChainPinecone(index, embedding_function=embeddings.embed_query, text_key="text")
 
     return vectorstore
