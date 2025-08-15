ed623e8b36b68324b5a5d0e79ab37ab2141cde5e
reeta-042 <155265356+reeta-042@users.noreply.github.com>
2025-08-15 05:35:27 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,4 +8,7 @@ load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_INDEX_NAME = "rag-bootcamp"
 
